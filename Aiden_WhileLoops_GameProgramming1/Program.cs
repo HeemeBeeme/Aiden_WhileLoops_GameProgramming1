bf4dc6af29c8a9ec53aa639f6281501bc56216c1
Heeme Beeme <heemebeeme@gmail.com>
2025-09-22 10:08:29 -0300

    Initial commit

---
+++ Aiden_WhileLoops_GameProgramming1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiden_WhileLoops_GameProgramming1
+{
+    internal class Program
+    {
+        static bool keepGuessing = false;
+        static bool gameStarted;
+        static int NumToGuess;
+
+        static Random GenerateNumber = new Random();
+
+        static void RandomNumber()
+        {
+            NumToGuess = GenerateNumber.Next(101);
+            Console.WriteLine("You Can Now Begin To Guess!");
+            keepGuessing = true;
+            PressAny();
+        }
+
+        static void PressAny()
+        {
+            Console.WriteLine("\nPress Any Key...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void PrintStart()
+        {
+            Console.WriteLine("Welcome to the higher or lower game!");
+            PressAny();
+            gameStarted = true;
+        }
+
+        static void GuessNumber()
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            PrintStart();
+
+            while(gameStarted)
+            {
+
+                RandomNumber();
+
+                while(keepGuessing)
+                {
+
+
+
+                }
+
+            }
+        }
+    }
+}
